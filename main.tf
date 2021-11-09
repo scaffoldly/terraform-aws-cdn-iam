09015bf0bf773ff885d9086f0d8686ad5ca3ebb7
Christian Nuss <christian.nuss@gmail.com>
2021-11-08 16:59:49 -0800

    IAM role for CDN deployments

---
+++ main.tf
@@ -0,0 +1,50 @@
+data "aws_iam_policy_document" "base" {
+  statement {
+    actions = [
+      "s3:List*",
+      "s3:Get*",
+      "s3:Describe*",
+      "s3:Put*",
+      "s3:Delete*",
+      "cloudfront:CreateInvalidation",
+    ]
+
+    resources = [
+      "arn:*:s3:::${var.repository_name}",
+      "arn:*:s3:::${var.repository_name}/*",
+      "arn:*:cloudfront::*:distribution/*",
+    ]
+  }
+
+  statement {
+    effect = "Deny"
+
+    actions = [
+      "s3:DeleteBucket",
+      "s3:DeleteObjectVersion"
+    ]
+
+    resources = [
+      "arn:*:s3:::${var.repository_name}",
+      "arn:*:s3:::${var.repository_name}/*",
+    ]
+  }
+}
+
+resource "aws_iam_user" "user" {
+  name = "${var.repository_name}-deployer"
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name   = "base-policy"
+  user   = aws_iam_user.user.name
+  policy = data.aws_iam_policy_document.base.json
+}
+
+resource "aws_iam_access_key" "access_key" {
+  user = aws_iam_user.user.name
+
+  depends_on = [
+    aws_iam_user_policy.policy
+  ]
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
-
+variable "repository_name" {
+  type        = string
+  description = "The repository name for the Public Webiste (role names are prefixed with this for service-level isololation of privileges)"
+}
